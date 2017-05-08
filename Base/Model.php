3f3ae4a448c74defb678ac872daf313f88c5d9eb
czhuyu <czhuyu@gmail.com>
2017-05-08 10:19:55 +0800

    分析表名/列名等并赋值到model的属性中

--- Base/Model.php
+++ Base/Model.php
@@ -8,13 +8,66 @@ namespace Z\Base;
 
 class Model{
 	protected $db;//获取数据库连接的pdo对象
-	protected $table_name;
-	protected $field;
+	protected $table_name = '';
+	protected $field = [];
 	protected $pk;
 
 	public function getTableName(){
 		$this->table_name = strtolower(substr(strrchr(get_called_class(), '\\'), 1, -5));
-		echo $this->table_name;
+	}
+
+	public function resolveTable(){
+		$this->getTableName();
+		$db = new \Z\Base\Db();
+		$desc = $db->getAll('desc '.$this->table_name,[]);
+		foreach ($desc as $v) {
+			if($v['Key']=='PRI'){
+				$this->pk = $v['Field'];
+			}
+			$this->field[] = $v['Field'];
+		}
+	}
+
+	/**
+	*查询一条数据
+	*
+	*/
+	public function find($pk){
+		return 
+	}
+
+	/**
+	*查询多条数据
+	*
+	*/
+	public function select(){
+
+	}
+
+
+
+	/**
+	*更新数据
+	*
+	*/
+	public function save(){
+
+	}
+
+	/**
+	*删除数据
+	*
+	*/
+	public function delete(){
+
+	}
+
+	/**
+	*增加数据
+	*
+	*/
+	public function add(){
+
 	}
 
 }
