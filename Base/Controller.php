758e1abf55f9ae8ac373b02f97d8aaa25a2d21c1
czhuyu <czhuyu@gmail.com>
2018-04-02 13:52:51 +0800

    some change

--- Base/Controller.php
+++ Base/Controller.php
@@ -1,33 +1,33 @@
-<?php  
+<?php
 
 /**
-*所有Controller的父类
-*/
+ *所有Controller的父类
+ */
+
 namespace Z\Base;
-class Controller{
-	protected $data = [];
+class Controller
+{
+    protected $data = [];
 
 
-	/**
-	*这里先将后台的数据放进数组_data里面
-	*@param $k 赋值的键 ; $v 赋值的值
-	*/
-	public function assign($k,$v){
-		$this->data[$k] = $v; 
-	}
+    /**
+     *这里先将后台的数据放进数组_data里面
+     * @param $k 赋值的键 ; $v 赋值的值
+     */
+    public function assign($k, $v)
+    {
+        $this->data[$k] = $v;
+    }
 
 
-	/**
-	*输出到模板
-	*@param $view 需要输出到的模板位置
-	*/
-	public function display($view){
-		extract($this->data,EXTR_OVERWRITE
-);//因为传过来的值已经被封成一个数组,所以先把数组拆成变量名=变量值lllaaa
-		include (APP_PATH.'/View/Index/'.$view.'.html');
-	}
+    /**
+     *输出到模板
+     * @param $view 需要输出到的模板位置
+     */
+    public function display($view)
+    {
+        //因为传过来的值已经被封成一个数组,所以先把数组拆成变量名=变量值lllaaasss
+        extract($this->data, EXTR_OVERWRITE);
+        include(APP_PATH . '/View/Index/' . $view . '.html');
+    }
 }
-
-
-
-?>
