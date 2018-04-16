9426b97b67278633a0048d58dd63906b6b3624a8
czhuyu <czhuyu@gmail.com>
2018-04-16 09:47:16 +0800

    test new branches

--- Zbase.php
+++ Zbase.php
@@ -5,7 +5,7 @@
 namespace Z;
 
 define('ZPATH', __DIR__);//定义框架的根目录
-define('DEBUG', false);//定义是否开启debug模式fasdfsafdfasd
+define('DEBUG', false);//定义是否开启debug模式fas
 
 class Zbase{
 	private static $app = null;
